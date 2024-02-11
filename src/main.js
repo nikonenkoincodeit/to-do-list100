88556650e566e48ad58972c7964897aeeab9d604
VitaPad <v.mars22@ukr.net>
2024-02-11 11:05:17 +0200

    add function onSubmit

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
+import { formEL } from "./refs";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/style.css";
+
+formEL.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const value = event.target.elements.message.value.trim();
+  event.target.reset();
+
+  /*   console.log(event.currentTarget); */
+}
---
+++ src/refs/index.js
@@ -0,0 +1,1 @@
+export const formEL = document.querySelector(".form");
